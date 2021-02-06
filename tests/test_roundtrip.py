6758ce0ee4e3e21b8d78ee85f8603c0cf77e99d9
Ivan Savov <ivan.savov@gmail.com>
2021-02-06 11:14:01 -0500

    Added another roundtrip test

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -1,6 +1,8 @@
 import difflib
 import os
 
+import petl
+
 from libtabular import fromcsvwithheader
 from libtabular import tocsvwithheader
 
@@ -33,3 +35,37 @@ def test_load_and_write(tmpdir):
     # print('---')
     # print(open(tempfile).read())
 
+
+
+def test_write_and_load(tmpdir):
+    """
+    Roundtrip test write CSV with metadata to disk then load CSV and check the
+    data and metadata is the same as the original.
+    """
+    # original
+    metadata = {
+        "key3": "value3",
+        "key4 (space)": "value4 (with brackets)",
+        "ключ5": "данни5",
+    }
+    header = ["f1", "f2 with spaces", "фийлд3 (non-ascii)", "field4, with comma"]
+    data = [
+        ('1', '2', '3', '4'),
+        ('вальо', 'Smith, John', '1', '1.1'),
+        ('other punct ;"*_f#[]%@!~`', 'val (val)', '', 'zzzzz')
+    ]
+
+    # write data to disk
+    tempfile = os.path.join(tmpdir, 'roundtrip_test.csv')
+    table = [header] + data
+    tocsvwithheader(table, tempfile, metadata=metadata)
+
+    # load table from disk
+    table = fromcsvwithheader(tempfile)
+
+    # checks
+    assert table.metadata == metadata, 'different metadata'
+    assert table.header == tuple(header), 'different header'
+    assert len(list(table.dicts())) == 3, 'wrong number of data rows'
+    for exp_row, obs_row in zip(data, petl.data(table)):
+        assert exp_row == obs_row, 'different data row encountered'
